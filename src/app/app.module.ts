75ffdb2db1be81c6d253629b2fa5eaa8f97ec697
vpfl95 <k_dh610@naver.com>
2020-08-24 12:01:27 +0900

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EcgComponent } from './ecg/ecg.component';
+import { RespirationComponent } from './respiration/respiration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EcgComponent,
+    RespirationComponent
   ],
   imports: [
     BrowserModule
